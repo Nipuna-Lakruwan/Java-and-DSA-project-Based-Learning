d2c9e58ca943f80afd7d55e212bbcb3cd62ebd69
Nipuna Lakruwan <laknipsa@gmail.com>
2024-10-21 01:49:09 +0530

    For Loop

---
+++ 12. For Loop/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+  public static void main(String[] args) {
+    
+
+    // For Loop = executes a block of code a limited amount of times
+    //      for (statement 1; statement 2; statement 3) { }
+    //      statement 1 = executed (one time) before the execution of the code block
+    //      statement 2 = defines the condition for executing the code block
+    //      statement 3 = executed (every time) after the code block has been executed
+
+    // Count from 0 to 10
+    // for (int i = 0; i <= 10; i++) {
+    //   System.out.println(i);
+    // }
+
+    // Happy New Year! 10..9..8..7..6..5..4..3..2..1..0..Happy New Year!
+    for (int i = 10; i >= 0; i--) {
+      System.out.println(i);
+  }
+  System.out.println("Happy New Year!");
+  }
+}
