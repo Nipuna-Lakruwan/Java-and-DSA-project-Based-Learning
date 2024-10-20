f199662707a9f8f4f9f4d6f07b7eb0d1518ffbb6
Nipuna Lakruwan <laknipsa@gmail.com>
2024-10-21 01:19:02 +0530

    If Statements

---
+++ 08. If Statement/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+  public static void main(String[] args) {
+    
+    // If Statement = perform a block of code if it's condition is true
+
+    int age = 80;
+
+    if (age >= 75) {
+      System.out.println("Ok Boomer!");
+    } else if (age >= 18) {
+      System.out.println("You are an adult!");
+    } else if (age >= 13) {
+      System.out.println("You are a teenager!");
+    } else {
+      System.out.println("You are not an adult!");
+    }
+  }
+  
+}
