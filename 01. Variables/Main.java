e835f09cee2ce2b6a88e03a80ec3080540df8268
Nipuna Lakruwan <laknipsa@gmail.com>
2024-10-21 00:21:59 +0530

    Variables

---
+++ 01. Variables/Main.java
@@ -0,0 +1,27 @@
+public class Main {
+  public static void main(String[] args) {
+    //int x; // Declare a variable x
+
+    //x = 10; // Assign the value 10 to x
+
+    int x = 10; // Declare and assign the value 10 to x
+
+    long y = 1000000000000000000L; // Declare and assign the value 1000000000000000000 to y
+
+    byte z = 127; // Declare and assign the value 127 to z
+
+    double d = 10.5; // Declare and assign the value 10.5 to d
+
+    float f = 10.5f; // Declare and assign the value 10.5 to f
+
+    boolean b = true; // Declare and assign the value true to b
+
+    char c = 'A'; // Declare and assign the value A to c
+
+    String s = "Hello World!"; // Declare and assign the value Hello World! to s
+
+    System.out.println("My number is " + f); // Print the value of x
+
+    System.out.println(s);
+  }
+}
