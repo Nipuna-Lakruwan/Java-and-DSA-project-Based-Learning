614d68c64c669ae7bc9b609de242b10937e0a028
Nipuna Lakruwan <laknipsa@gmail.com>
2024-10-31 00:08:49 +0530

    Add Main class to demonstrate access modifiers in Java

---
+++ 25. OOP/13. Access Modifiers/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+  public static void main(String[] args) {
+    
+    // Access Modifiers
+    // - public = accessible from anywhere (within the class, outside the class, within the package, outside the package) 
+    // - private = accessible only within the class 
+    // - protected = accessible within the class, within the package, and by subclasses 
+    // - default = accessible only within the package (no keyword) 
+    
+  }
+}
