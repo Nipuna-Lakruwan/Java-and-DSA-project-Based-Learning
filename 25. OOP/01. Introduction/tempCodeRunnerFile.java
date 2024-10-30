47f25a87c9e73753ec01ca82f5a4397dfefcd648
Nipuna Lakruwan <laknipsa@gmail.com>
2024-10-30 17:24:26 +0530

    Add temporary code runner file with commented-out Car attributes

---
+++ 25. OOP/01. Introduction/tempCodeRunnerFile.java
@@ -0,0 +1,5 @@
+      // System.out.println(myCar.make);
+      // System.out.println(myCar.model);
+      // System.out.println(myCar.year);
+      // System.out.println(myCar.color);
+      // System.out.println(myCar.price);
