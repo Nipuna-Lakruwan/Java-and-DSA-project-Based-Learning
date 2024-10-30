7015331e7f8ce22968fb3ffb5c2628dd645f2a0d
Nipuna Lakruwan <laknipsa@gmail.com>
2024-10-30 17:24:08 +0530

    Add Main class demonstrating basic OOP concepts and Car object instantiation

---
+++ 25. OOP/01. Introduction/Main.java
@@ -0,0 +1,42 @@
+public class Main {
+  public static void main(String[] args) {
+      
+      // OOP
+      // - Object Oriented Programming
+      // - a programming model based on objects
+      // - objects = instance of classes
+      // - classes = blueprint for objects
+      // - OOP principles = inheritance, encapsulation, polymorphism, abstraction
+  
+      // class = a blueprint for objects
+      // object = instance of a class
+  
+      // class = data + methods
+      // data = variables (fields)
+      // methods = functions
+  
+      // class = blueprint
+      // object = instance of the class
+
+      Car myCar = new Car();
+      Car myCar2 = new Car();
+
+      
+      // System.out.println(myCar.make);
+      // System.out.println(myCar.model);
+      // System.out.println(myCar.year);
+      // System.out.println(myCar.color);
+      // System.out.println(myCar.price);
+
+      System.out.println(myCar2.make);
+      System.out.println(myCar2.model);
+      System.out.println(myCar2.year);
+      System.out.println(myCar2.color);
+      System.out.println(myCar2.price);
+      
+
+      //myCar.drive();
+      //myCar.brake();
+
+  }
+}
